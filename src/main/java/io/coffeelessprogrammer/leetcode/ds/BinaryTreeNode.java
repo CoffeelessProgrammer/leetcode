a1106b9945ecb7229579b2d7beb397a65724e759
Ryo112358 <11039459+Ryo112358@users.noreply.github.com>
2021-07-13 11:38:31 -0400

    Solved 'FindCorrNodeInBinaryTreeClone'

---
+++ src/main/java/io/coffeelessprogrammer/leetcode/ds/BinaryTreeNode.java
@@ -0,0 +1,8 @@
+package io.coffeelessprogrammer.leetcode.ds;
+
+public class BinaryTreeNode {
+    public int val;
+    public BinaryTreeNode left;
+    public BinaryTreeNode right;
+    public BinaryTreeNode(int x) { val = x; }
+}
