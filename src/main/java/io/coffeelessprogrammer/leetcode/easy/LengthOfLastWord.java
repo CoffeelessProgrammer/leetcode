935cd5fa8a5cf2879e515844545b5b1bcaa02435
OmnicWolf <87935417+OmnicWolf@users.noreply.github.com>
2021-09-15 13:48:39 -0400

    Completed 'Length of Last Word'

---
+++ src/main/java/io/coffeelessprogrammer/leetcode/easy/LengthOfLastWord.java
@@ -0,0 +1,33 @@
+package io.coffeelessprogrammer.leetcode.easy;
+
+/*
+ * Problem: 58. Length of Last Word
+ * Acceptance Rate: 34.4%
+ * URL: https://leetcode.com/problems/length-of-last-word/
+ *
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for Length of Last Word.
+ * Memory Usage: 37.3 MB, less than 66.93% of Java online submissions for Length of Last Word.
+ */
+
+public class LengthOfLastWord {
+
+    public static int find(String s) {
+        int lengthOfLastWord = 0;
+
+        boolean firstLetterFound = false;
+
+        for(int i=s.length()-1; i > -1; --i) {
+            if(firstLetterFound) {
+                if(s.charAt(i) != ' ')
+                    ++lengthOfLastWord;
+                else
+                    break;
+            } else if(s.charAt(i) != ' ') {
+                ++lengthOfLastWord;
+                firstLetterFound = true;
+            }
+        }
+
+        return lengthOfLastWord;
+    }
+}
---
+++ src/test/java/io/coffeelessprogrammer/leetcode/easy/LengthOfLastWordTests.java
@@ -0,0 +1,23 @@
+package io.coffeelessprogrammer.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LengthOfLastWordTests {
+
+    @Test
+    final void HelloWorld_5() {
+        assertEquals(5, LengthOfLastWord.find("Hello World"));
+    }
+
+    @Test
+    final void HelloWorld_5_TrailingSpaces() {
+        assertEquals(5, LengthOfLastWord.find("Hello World  "));
+    }
+
+    @Test
+    final void HelloWorld_5_LeadingSpaces() {
+        assertEquals(5, LengthOfLastWord.find("  Hello World  "));
+    }
+}
