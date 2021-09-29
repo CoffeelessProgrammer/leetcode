7a6b4905ec9f9d2b9ccc07fac2292fa1624fea79
OmnicWolf <87935417+OmnicWolf@users.noreply.github.com>
2021-09-29 13:06:24 -0400

    String reduction optimization for valid palindrome
    Runtime improvement: 31% → 98%

--- src/main/java/io/coffeelessprogrammer/leetcode/ds/ListNode.java
+++ src/main/java/io/coffeelessprogrammer/leetcode/ds/ListNode.java
@@ -3,6 +3,7 @@ package io.coffeelessprogrammer.leetcode.ds;
 public class ListNode {
     public int val;
     public ListNode next;
+
     public ListNode() {}
     public ListNode(int val) { this.val = val; }
     public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
--- src/main/java/io/coffeelessprogrammer/leetcode/easy/ValidPalindrome.java
+++ src/main/java/io/coffeelessprogrammer/leetcode/easy/ValidPalindrome.java
@@ -7,6 +7,11 @@ package io.coffeelessprogrammer.leetcode.easy;
  *
  * Runtime: 21 ms, faster than 31.51% of Java online submissions for Valid Palindrome.
  * Memory Usage: 40 MB, less than 47.84% of Java online submissions for Valid Palindrome.
+ *
+ *   ↓ Switched from regex to custom string reduction algorithm
+ *
+ * Runtime: 2 ms, faster than 98.45% of Java online submissions for Valid Palindrome.
+ * Memory Usage: 39.2 MB, less than 62.14% of Java online submissions for Valid Palindrome.
  */
 
 public class ValidPalindrome {
@@ -26,6 +31,25 @@ public class ValidPalindrome {
     }
 
     private static String reduceToAlphaNumeric(String str) {
+        // 0 to 9 = 48-57
+        // A to Z = 65-90
+        // a to z = 97-122
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i < str.length(); ++i) {
+            char curr = str.charAt(i);
+
+            if((curr < 123 && curr > 96) || (curr < 58 && curr > 47)) {
+                sb.append(curr);
+            } else if(curr < 91 && curr > 64) {
+                sb.append((char) (curr+32));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private static String reduceToAlphaNumericRegex(String str) {
         return str.toLowerCase().replaceAll("[^a-z0-9]", "");
     }
 
